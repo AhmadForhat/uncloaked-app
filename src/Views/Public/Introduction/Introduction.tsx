f595f5a7160f161a17f8b4942c00b7daa6ef5f01
ahmad.forhat <ahmad.forhat@welbe.mx>
2023-02-21 14:44:33 -0800

    refactor: :recycle: test to button download pwa

--- src/Views/Public/Introduction/Introduction.tsx
+++ src/Views/Public/Introduction/Introduction.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+interface deferredPromptProps {
+	prompt: () => void;
+	userChoice: { outcome: null };
+}
 
 const Introduction = () => {
-	return <div data-testid="introduction-container">Introduction</div>;
+	const [isOpenButton, setOpenButton] = useState(false);
+	const deferredPrompt = useRef<deferredPromptProps | null>(null);
+
+	useEffect(() => {
+		window.addEventListener('beforeinstallprompt', (e: any) => {
+			setOpenButton(true);
+			deferredPrompt.current = e;
+		});
+	}, []);
+
+	const handleClick = async () => {
+		if (deferredPrompt.current !== null) {
+			deferredPrompt.current.prompt();
+			const { outcome } = await deferredPrompt.current.userChoice;
+			if (outcome === 'accepted') {
+				deferredPrompt.current = null;
+			}
+		}
+	};
+
+	return (
+		<div data-testid="introduction-container">
+			Introduction
+			{isOpenButton && <button onClick={handleClick}>Download app!</button>}
+		</div>
+	);
 };
 
 export default Introduction;
