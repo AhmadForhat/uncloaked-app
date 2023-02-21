eb7f081f6d9da686b2733901a1fb39967bed0f82
ahmad.forhat <ahmad.forhat@welbe.mx>
2023-02-21 14:47:49 -0800

    refactor: :recycle: change the version

--- src/Views/Public/Introduction/Introduction.tsx
+++ src/Views/Public/Introduction/Introduction.tsx
@@ -1,37 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
-
-interface deferredPromptProps {
-	prompt: () => void;
-	userChoice: { outcome: null };
-}
+import React from 'react';
 
 const Introduction = () => {
-	const [isOpenButton, setOpenButton] = useState(false);
-	const deferredPrompt = useRef<deferredPromptProps | null>(null);
-
-	useEffect(() => {
-		window.addEventListener('beforeinstallprompt', (e: any) => {
-			setOpenButton(true);
-			deferredPrompt.current = e;
-		});
-	}, []);
-
-	const handleClick = async () => {
-		if (deferredPrompt.current !== null) {
-			deferredPrompt.current.prompt();
-			const { outcome } = await deferredPrompt.current.userChoice;
-			if (outcome === 'accepted') {
-				deferredPrompt.current = null;
-			}
-		}
-	};
-
-	return (
-		<div data-testid="introduction-container">
-			Introduction
-			{isOpenButton && <button onClick={handleClick}>Download app!</button>}
-		</div>
-	);
+	return <div data-testid="introduction-container">Introduction</div>;
 };
 
 export default Introduction;
